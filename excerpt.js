115381ec9a772b0703944a83be811acd233bd5e7
Kevin Zweerink <kevin@kevinzweerink.com>
2013-03-24 16:46:58 -0400

    basic selection detection

--- excerpt.js
+++ excerpt.js
@@ -1,1 +1,25 @@
-alert("connection established");
+(function(){
+	var selectedText = function() {
+		var text = "";
+		if (window.getSelection) {
+			text = window.getSelection();
+		} else if (document.getSelection) {
+			text = document.getSelection();
+		} else if (document.selection) {
+			text = document.selection.createRange().text;
+		}
+		
+		return text;
+	}
+	
+	var selection = selectedText();
+	
+	if (selection != "") {
+		alert (selection);
+	} else {
+		alert ("nothing selected");
+	}
+	
+	
+	
+})();
